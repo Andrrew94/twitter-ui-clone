511f862e7007b11fdd6d9631a5695e1f7eda28df
Tedi <tedi.turluianu@gmail.com>
2020-07-01 17:54:04 +0300

    Landing Page created

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import LandingPage from './components/LandingPage/LandingPage';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <br />
-        <strong> Turluianu Tedi</strong>
-        <strong> Andrei <strong style={{color: "green"}}>Inginer</strong> Sandica</strong>
-      </header>
-    </div>
+   <LandingPage />
   );
 }
 
---
+++ src/components/LandingPage/LandingPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './LandingPage.scss';
+
+const LandingPage = () => (
+  <div className="LandingPage" data-testid="LandingPage">
+    <strong>LandingPage</strong>
+  </div>
+);
+
+LandingPage.propTypes = {};
+
+LandingPage.defaultProps = {};
+
+export default LandingPage;
