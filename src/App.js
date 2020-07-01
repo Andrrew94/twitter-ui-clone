30b5d22dabe4a43f1f88c0039f5236cb6536dd4d
Tedi <tedi.turluianu@gmail.com>
2020-07-01 16:44:03 +0300

    testing

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <br />
+        <strong style="color:red"> Turluianu Tedi</strong>
         <a
           className="App-link"
           href="https://reactjs.org"
