40f4b229a56ef64cac172f1caeec555aee6990cf
Tedi <tedi.turluianu@gmail.com>
2020-07-03 17:20:22 +0300

    added axios instance

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
+import Header from './Components/Header/Header';
+import mainAxios from './instances/axiosInstance';
 
 const App = ()=> {
+  mainAxios.get(`/post`).then((res)=> {
+    console.log(res.data)
+  })
   return (
-    <div className="container" style={{backgroundColor: "red", height: "100vh", color: "white"}}>
+    <div>
+      <Header />
+      <div className="container" style={{backgroundColor: "red", height: "100vh", color: "white"}}>
       UI is working
+     </div>
     </div>
   );
 }
